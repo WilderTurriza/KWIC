320ec86b19916d1b45ad716f9125ed7b3caf95b3
Wilder Turriza <turriza.poot.wilder@gmail.com>
2022-09-24 15:15:27 -0500

    Quitanto el encabezado

--- Arq_kwic/src/CapaVista/Principal.java
+++ Arq_kwic/src/CapaVista/Principal.java
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-=======
->>>>>>> ramaIbis
+
 package CapaVista;
 
 public class Principal {
